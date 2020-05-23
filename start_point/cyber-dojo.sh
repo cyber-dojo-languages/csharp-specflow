fa76f3564111fdd7b2fc7457b50b40b62fdaf72f
Jon Jagger <jon@jaggersoft.com>
2020-05-23 12:29:00 +0100

    Remove hidden_filenames from manifest; add trap handler in cyber-dojo.sh instead

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,29 +1,69 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------
+# cyber-dojo returns text files under /sandbox that are
+# created/deleted/changed. In tidy_up you can remove any
+# such files you don't want returned to the browser.
+
+trap tidy_up EXIT
+
+function tidy_up()
+{
+  delete_files TestResult.xml
+  delete_files TechTalk.SpecFlow.targets
+  delete_files TechTalk.SpecFlow.tasks
+  delete_files TechTalk.SpecFlow.props
+  delete_files specflow.json
+  delete_files specflow.exe.config
+  delete_files plugincompability.config
+  delete_files app.config
+  delete_files RunTests.csproj
+  delete_files *.feature.cs
+}
+
+function delete_files()
+{
+  for filename in "$@"
+  do
+      rm "${filename}" 2> /dev/null || true
+  done
+}
+
+# ------------------------------------------------------------------------
 # build project file
-CSPROJ=RunTests.csproj
+{
+  echo "<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">"
+  echo "  <ItemGroup>"
+  for file in *.feature
+  do
+    echo "    <None Include=\"${file}\">"
+    echo "      <Generator>SpecFlowSingleFileGenerator</Generator>"
+    echo "      <LastGenOutput>${file}.cs</LastGenOutput>"
+    echo "    </None>"
+  done
+  echo "  </ItemGroup>"
+  echo "  <ItemGroup>"
+  echo "    <None Include=\"specflow.json\" />"
+  echo "  </ItemGroup>"
+  echo "</Project>"
+} > RunTests.csproj
 
-echo "<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">" > $CSPROJ
-echo "  <ItemGroup>" >> $CSPROJ
-
-for f in *.feature
-do
-  echo "    <None Include=\"$f\">" >> $CSPROJ
-  echo "      <Generator>SpecFlowSingleFileGenerator</Generator>" >> $CSPROJ
-  echo "      <LastGenOutput>$f.cs</LastGenOutput>" >> $CSPROJ
-  echo "    </None>" >> $CSPROJ
-done
-
-echo "  </ItemGroup>" >> $CSPROJ
-echo "  <ItemGroup>" >> $CSPROJ
-echo "    <None Include=\"specflow.json\" />" >> $CSPROJ
-echo "  </ItemGroup>" >> $CSPROJ
-echo "</Project>" >> $CSPROJ
+# build specflow.json
+cat << EOF > specflow.json
+{
+  "specflow": {
+    "runtime": {"missingOrPendingStepsOutcome": "Error"},
+    "unitTestProvider": {"name": "NUnit"}
+  }
+}
+EOF
 
 NUNIT_PATH=/nunit/lib/net45
 export MONO_PATH=${NUNIT_PATH}
 
-# generate 'code behind'
+# ------------------------------------------------------------------------
+# generate 'code behind' and run
+
 find . -iname '*.feature.cs' -exec rm '{}' \;
 mono /specflow/specflow.exe generateall RunTests.csproj
 
